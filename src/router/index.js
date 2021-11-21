936c62b1d1b8376859a0cadfd98f8c1162dcc81d
PGabriel20 <bielporreca@gmail.com>
2021-11-21 19:36:54 -0300

    Removed component names

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -14,7 +13,6 @@ const routes = [
   },
   {
     path: '/login',
-    name: 'Login',
     component: () => import(/* webpackChunkName: "about" */ '../views/login/Login')
   }
 ]
