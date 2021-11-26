38dcbf96b3a4fb9998860898c049adef515764e8
PGabriel20 <bielporreca@gmail.com>
2021-11-25 21:56:39 -0300

    Fixing firebase configuration

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/compat/app'
+import { initializeApp } from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/storage'
 import 'firebase/database'
 
 const env = process.env
 
-export const firebaseApp = firebase.initializeApp({
-  apiKey: env.API_KEY,
-  authDomain: env.API_KEY,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSAGING_SENDER_ID,
-  appId: env.APP_ID
+export const firebaseApp = initializeApp({
+  apiKey: env.VUE_APP_API_KEY,
+  authDomain: env.VUE_APP_API_KEY,
+  projectId: env.VUE_APP_PROJECT_ID,
+  storageBucket: env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: env.VUE_APP_APP_ID
 })
 
 export default function install (Vue) {
