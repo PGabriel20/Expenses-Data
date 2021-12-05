a6cfda9f693417668be63bf45d4c7b95ecffe55d
PGabriel20 <bielporreca@gmail.com>
2021-12-05 17:39:57 -0300

    Removing redundant css class

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -6,7 +6,7 @@
   <div class="w-64 navigationMenu text-white">
       <router-link
         :key="i"
-        class="mb-2 mt-2 p-2 pl-4 flex items-center hover:bg-featured hover:text-dark transition duration-150 cursor-pointer"
+        class="mb-2 mt-2 p-2 pl-4 flex items-center hover:bg-featured hover:text-dark transition duration-150"
         v-for="(router, i) in routerLinks"
         :to="{ name: router.name }"
         exact-active-class="text-featured"
