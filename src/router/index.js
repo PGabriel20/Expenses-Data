158db57ba3ca7f95cc678db410fcb71bc89592e0
PGabriel20 <bielporreca@gmail.com>
2021-12-05 17:35:59 -0300

    Added new titles and icons for each route

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    meta: {
+      icon: 'home', title: 'Home'
+    },
     component: () => import(/* webpackChunkName: "about" */ '../views/home/Home')
   },
   {
     name: 'login',
     path: '/login',
+    meta: { title: 'Login' },
     component: () => import(/* webpackChunkName: "about" */ '../views/login/Login')
+  },
+  {
+    name: 'lista-gastos',
+    path: '/lista-gastos  ',
+    meta: { title: 'Lista Gastos', icon: 'list-ul' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/lista-gastos/ListaGastos')
   }
 ]
 
@@ -24,6 +34,8 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  document.title = `Expenses - ${to.meta.title}`
+
   if (!window.uid && to.name !== 'login') {
     next({ name: 'login' })
   } else {
