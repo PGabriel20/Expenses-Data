a6a41f6045c4712a68bd0e66415a2d0086950afa
PGabriel20 <bielporreca@gmail.com>
2021-12-05 17:35:26 -0300

    Added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="app" class="bg-darker min-h-screen text-light">
+  <div id="app" class="bg-darker min-h-screen flex w-full text-light">
+    <navigation v-if="isLoggedIn"/>
+    <div class="w-full bg-black">
+      <router-view/>
+    </div>
     <base-spinner></base-spinner>
-    <router-view/>
   </div>
 </template>
 
 <script>
 import BaseSpinner from './components/global/BaseSpinner'
+import Navigation from './components/layout/Navigation'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default {
   components: {
-    BaseSpinner
+    BaseSpinner,
+    Navigation
+  },
+  data () {
+    return {
+      isLoggedIn: false
+    }
   },
   mounted () {
     const auth = getAuth()
 
     onAuthStateChanged(auth, (user) => {
       window.uid = user ? user.uid : null
+
+      this.isLoggedIn = !!user
 
       if (!window.uid) {
         this.$router.push({
---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="bg-darkMedium h-screen">
+  <div class="mb-4 mt-4 text-xl flex justify-center ">
+    <h1>Expenses</h1>
+  </div>
+  <div class="w-64 navigationMenu text-white">
+      <router-link
+        :key="i"
+        class="mb-2 mt-2 p-2 pl-4 flex items-center hover:bg-featured hover:text-dark transition duration-150 cursor-pointer"
+        v-for="(router, i) in routerLinks"
+        :to="{ name: router.name }"
+        exact-active-class="text-featured"
+      >
+        <i class="fa mr-2" :class="`fa-${router.meta.icon}`"></i>
+        {{ router.meta.title }}
+      </router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    routerLinks () {
+      return this.$router.options.routes.filter(route => route.name !== 'login')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
