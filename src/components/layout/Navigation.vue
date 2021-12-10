c8b6b7b6bc9d85f988ba0a9e61be95fdf1879704
PGabriel20 <bielporreca@gmail.com>
2021-12-10 18:52:05 -0300

    Importing new components plus some styling

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="bg-darkMedium h-screen">
-  <div class="mb-4 mt-4 text-xl flex justify-center ">
-    <h1>Expenses</h1>
-  </div>
-  <div class="w-64 navigationMenu text-white">
+<nav class="bg-darkMedium h-screen flex flex-col justify-between">
+  <div>
+    <div class="mb-4 mt-4 text-xl flex justify-center ">
+      <h1>Expenses</h1>
+    </div>
+    <div class="w-64 navigationMenu text-white">
       <router-link
         :key="i"
         class="mb-2 mt-2 p-2 pl-4 flex items-center hover:bg-featured hover:text-dark"
@@ -14,12 +15,23 @@
         <i class="fa mr-2" :class="`fa-${router.meta.icon}`"></i>
         {{ router.meta.title }}
       </router-link>
+      <hr class="border-darkLow mt-4 mb-4 m-1">
+    </div>
+    <new-expense></new-expense>
   </div>
-</div>
+  <logout></logout>
+</nav>
 </template>
 
 <script>
+import Logout from './Logout'
+import NewExpense from './NewExpense'
+
 export default {
+  components: {
+    Logout,
+    NewExpense
+  },
   computed: {
     routerLinks () {
       return this.$router.options.routes.filter(route => route.name !== 'login')
@@ -29,5 +41,4 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-
 </style>
